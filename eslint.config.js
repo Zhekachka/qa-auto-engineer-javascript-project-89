4b5ce7c6a6edcdbdf460038f31b40daeef869ba7
EvgeniyaKokareva <zhekachkakokareva@yandex.ru>
2025-11-12 11:43:19 +0300

    Попытка настроить линтер

--- eslint.config.js
+++ eslint.config.js
@@ -5,14 +5,16 @@ import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
   stylistic.configs.recommended,
-  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.{js,mjs,cjs}'], languageOptions: {
-    globals: {
-      ...globals.browser,
-      ...globals.node,
-      ...globals.jest,
-      window: 'readonly',
+  { files: ['**/*.{js,mjs,cjs,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+        window: 'readonly',
+      },
     },
-  },
   },
 ])
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom/client'
 import '@hexlet/chatbot-v2/styles'
 import App from './App'
 
-const container = document.getElementById('root');
+const container = document.getElementById('root')
 ReactDOM.createRoot(container)
-  .render(<App/>);
+  .render(<App/>)
